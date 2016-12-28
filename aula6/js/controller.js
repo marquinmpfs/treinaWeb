243552068e4f7f603f298c76ae35934dbd7e2cd1
Marcos Ferreira <marcos.silva@eldorado.org.br>
2016-12-28 08:28:05 -0200

    Finalizado Parte 1

--- aula6/js/controller.js
+++ aula6/js/controller.js
@@ -2,6 +2,27 @@ angular.module("minhaPizza")
 
 .controller("novoSaborCtrl", ['$scope', function ($scope) {
 
-	$scope.nome = "TreinaWeb";
-	
+	$scope.Pizza = {
+		nome: "",
+		formato: "Redonda",
+		local: "Entrega",
+		endereco: "",
+		ingredientes: [],
+		total: 0
+	};
+
+	$scope.Ingrediente = {
+		novo: "",
+		inserir: function(){
+			$scope.Pizza.ingredientes.push(this.novo);
+			this.novo = "";
+		},
+		remover: function(index){
+			$scope.Pizza.ingredientes.splice(index, 1);
+		}
+	}
+
+	$scope.formatoPizza = function () {
+		return 'pizza-formato ' + ($scope.Pizza.formato === "Redonda" ? "pizza-redonda" : "pizza-quadrada");
+	}
 }]);
